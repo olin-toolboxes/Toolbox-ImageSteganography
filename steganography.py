26bb0569487bcc1300939002791e216e9cd06050
flxbrhrdt <31519444+flxbrhrdt@users.noreply.github.com>
2017-10-26 15:31:00 -0400

    Does not decode how it should

--- steganography.py
+++ steganography.py
@@ -8,7 +8,7 @@ def decode_image(file_location="images/encoded_sample.png"):
     file_location: the location of the image file to decode. By default is the provided encoded image in the images folder
     """
     encoded_image = Image.open(file_location)
-    red_channel = encoded_image.split()[0]
+    red_channel = encoded_image.split()[0] # isolate the red_channel from the original RGB image.
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
@@ -16,9 +16,16 @@ def decode_image(file_location="images/encoded_sample.png"):
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
 
-    pass #TODO: Fill in decoding functionality
+    # iterate though each pixel in the encoded image red_channel
+    for i in range(0,x_size):
+        for j in range(0,y_size):
+            # set the decode_image pixel to be (0, 0, 0) or (255, 255, 255)
+            if red_channel.getpixel((i,j)) == 1:
+                decoded_image.putpixel((i,j),(0,0,0))
+            elif red_channel.getpixel((i,j)) == 0:
+                decoded_image.putpixel((i,j),(255,255,255))
 
-    decoded_image.save("images/decoded_image.png")
+    decoded_image.save("images/decoded_image_1.png")
 
 def write_text(text_to_write, image_size):
     """Writes text to an RGB image. Automatically line wraps
@@ -43,11 +50,31 @@ def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
     text_to_encode: the text to encode into the template image
     template_image: the image to use for encoding. An image is provided by default.
     """
-    pass #TODO: Fill out this function
+    encoded_image = Image.open(template_image)
+    red_channel = encoded_image.split()[0] # isolate the red_channel from the original RGB image.
+    x_size = encoded_image.size[0]
+    y_size = encoded_image.size[1]
+
+    # Create a black and white picture
+    text_to_write = text_to_encode
+    image_size = encoded_image.size
+    text_decode = write_text(text_to_write, image_size)
+    # iterate through the picture to encode it
+    for i in range(0,x_size):
+        for j in range(0,y_size):
+            # set the decode_image pixel to be (0, 0, 0) or (255, 255, 255)
+            if text_decode.getpixel((i,j)) == (0,0,0):
+                red_channel.putpixel((i,j),0)
+            elif text_decode.getpixel((i,j)) == (255,255,255):
+                red_channel.putpixel((i,j),1)
+
+    encoded_image.save("images/encoded_image.png")
+    # text_decode.save("images/decoded_text.png")
+    # decode_image("images/encoded_image.png")
 
 if __name__ == '__main__':
-    print("Decoding the image...")
-    decode_image()
+    # print("Decoding the image...")
+    # decode_image()
 
     print("Encoding the image...")
-    encode_image()
+    encode_image('Test')
