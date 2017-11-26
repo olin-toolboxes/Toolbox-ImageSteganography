d9e2edac6dd264b8801736e2d82aa22d1a93eac2
Isabel Blancett <isabel.blancett@students.olin.edu>
2017-11-26 17:32:02 -0500

    Completed toolbox

--- steganography.py
+++ steganography.py
@@ -2,13 +2,14 @@
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
-def decode_image(file_location="images/encoded_sample.png"):
+def decode_image(file_dest, file_location="images/encoded_sample.png"):
     """Decodes the hidden message in an image
 
     file_location: the location of the image file to decode. By default is the provided encoded image in the images folder
     """
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
+    im = red_channel.load()
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
@@ -16,9 +17,12 @@ def decode_image(file_location="images/encoded_sample.png"):
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
 
-    pass #TODO: Fill in decoding functionality
+    for i in range(x_size):
+        for j in range(y_size):
+            val = int(format(im[i, j], '08b')[-1])
+            pixels[i, j] = (255*val, 255*val, 255*val)
 
-    decoded_image.save("images/decoded_image.png")
+    decoded_image.save(file_dest)
 
 def write_text(text_to_write, image_size):
     """Writes text to an RGB image. Automatically line wraps
@@ -43,11 +47,34 @@ def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
     text_to_encode: the text to encode into the template image
     template_image: the image to use for encoding. An image is provided by default.
     """
-    pass #TODO: Fill out this function
+    picture = Image.open(template_image)
+    red_channel = picture.split()[0]
+    green_channel = picture.split()[1]
+    blue_channel = picture.split()[2]
+    red_pixels = red_channel.load()
+    green_pixels = green_channel.load()
+    blue_pixels = blue_channel.load()
+
+    x_size = picture.size[0]
+    y_size = picture.size[1]
+
+    text = write_text(text_to_encode, picture.size).split()[0].load()
+
+    encoded_image = Image.new("RGB", picture.size)
+    pixels = encoded_image.load()
+
+    for i in range(x_size):
+        for j in range(y_size):
+            if int(format(red_pixels[i, j], '08b')[-1]) != int(format(text[i, j], '08b')[-1]):
+                pixels[i,j] = (abs(red_pixels[i, j]-1), green_pixels[i, j], blue_pixels[i, j])
+            else:
+                pixels[i,j] = (red_pixels[i, j], green_pixels[i, j], blue_pixels[i, j])
+
+    encoded_image.save("images/my_image.png")
+
 
 if __name__ == '__main__':
-    print("Decoding the image...")
-    decode_image()
-
     print("Encoding the image...")
-    encode_image()
+    encode_image("Hello World!")
+    print("Decoding the image...")
+    decode_image("images/my_message.png", "images/my_image.png")
